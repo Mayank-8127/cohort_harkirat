747f3f2aac3271c460faf2baeda88ebdfd133c7d
Mayank-8127 <145133998+Mayank-8127@users.noreply.github.com>
2024-03-16 16:33:42 +0530

    Add practice week-1 greet_gender.js

---
+++ practice/week-1/greet_gender.js
@@ -0,0 +1,9 @@
+let firstName = "Shakti";
+//let gender = "male";
+let gender = "female";
+if (gender == "male"){
+    console.log("Hello, Mr." + firstName);
+}
+if (gender == "female"){
+    console.log("Hello, Mrs." + firstName);
+}
