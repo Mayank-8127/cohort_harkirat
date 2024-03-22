522e4be1087989f06eb6c95bd10ee6909ca4234d
Mayank-8127 <145133998+Mayank-8127@users.noreply.github.com>
2024-03-23 01:17:02 +0530

    Complete assignments/easy/expenditure-analysis.js

--- assignments/01-js/easy/expenditure-analysis.js
+++ assignments/01-js/easy/expenditure-analysis.js
@@ -1,20 +1,56 @@
 /*
-  Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
-  and return a list of objects where each object is unique category-wise and has total price spent as its value.
-  transactions is an array where each
-  Transaction - an object like 
-        {
+	Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
+	and return a list of objects where each object is unique category-wise and has total price spent as its value.
+	transactions is an array where each
+	Transaction - an object like 
+				{
 		id: 1,
 		timestamp: 1656076800000,
 		price: 10,
 		category: 'Food',
 		itemName: 'Pizza',
 	}
-  Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
+	Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
-
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+	let arr = [];
+	let obj = {
+		category:undefined,
+		totalSpent:0
+	}
+	for(i = 0; i < transactions.length; i++){
+		let cat = transactions[i]["category"];
+		let arrCat;
+		if(arr.length == 0){
+			arr.push({
+				category:undefined,
+				totalSpent:0
+			});
+			arr[arr.length - 1]["category"] = transactions[i]["category"];
+			arr[arr.length - 1]["totalSpent"] += transactions[i]["price"];
+		}
+		else{
+			if(arr.length > 0){
+				for(j = 0; j < arr.length; j++){
+					arrCat = arr[j]["category"];
+					if(arrCat == cat){
+						arr[j]["totalSpent"] += transactions[i]["price"];
+                        break;
+					}else{
+						if(j == arr.length - 1){
+							arr.push({
+								category:undefined,
+								totalSpent:0
+							});
+							arr[arr.length - 1]["category"] = transactions[i]["category"];
+							arr[arr.length - 1]["totalSpent"] += transactions[i]["price"];
+							break;
+						}
+					}
+				}
+			} 
+		}
+	} 
+	return arr;
 }
-
 module.exports = calculateTotalSpentByCategory;
