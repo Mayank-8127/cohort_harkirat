470a141ea25d691f197bb2f81c92c28a6fabe6fe
Mayank-8127 <145133998+Mayank-8127@users.noreply.github.com>
2024-03-23 02:39:26 +0530

    Complete assignments/01-js/medium/times.js

--- assignments/01-js/medium/times.js
+++ assignments/01-js/medium/times.js
@@ -9,5 +9,20 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
+    let time1 = new Date();
+    let a = time1.getTime();
+    let sum = 0;
+    for(i = 1; i <= n; i++){
+        sum += i;
+    }
+    let time2 = new Date();
+    let b = time2.getTime();
+    console.log("Sum from 1-" + n + " is: " + sum);
+    console.log("Time taken to calculate sum from 1-" + n + " is: " + (b-a) + "ms");
     return 0.01;
 }
+calculateTime(100);
+calculateTime(100000);
+calculateTime(1000000000);
+calculateTime(10000000000);
+//values shown for larger sums is not accurate as it is beyond the limit of the variables.
