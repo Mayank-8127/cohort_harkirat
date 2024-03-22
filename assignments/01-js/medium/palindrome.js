c2f4c7077472626621eb3e55b4c1183f2dd069d6
Mayank-8127 <145133998+Mayank-8127@users.noreply.github.com>
2024-03-23 02:07:33 +0530

    Complete assignments/01-js/medium/palindrome.js

--- assignments/01-js/medium/palindrome.js
+++ assignments/01-js/medium/palindrome.js
@@ -3,7 +3,16 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+//Can be improved by making it compare only for alphabets instead of just excluding some symbols.
 function isPalindrome(str) {
+  let str1 = str.toLowerCase().split(" ").join('').split(",").join('').split(".").join('').split("?").join('').split("!").join('');
+  for(i = 0; i < str1.length; i++){
+    if(str1[i] == str1[str1.length - 1 - i]){
+    }
+    else{
+      return false;
+    }
+  }
   return true;
 }
 
