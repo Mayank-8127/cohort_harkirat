ce420f88c7273d17e7e366ad40e79453cef265d5
Mayank-8127 <145133998+Mayank-8127@users.noreply.github.com>
2024-03-23 14:35:35 +0530

    Complete assignments/01-js/hard/todo-list.js

--- assignments/01-js/hard/todo-list.js
+++ assignments/01-js/hard/todo-list.js
@@ -11,7 +11,45 @@
 */
 
 class Todo {
-
+  constructor(){
+    this.todos=[];
+  }
+  add(todo){
+    this.todos.push(todo);
+  }
+  remove(index){
+    if(index >= this.todos.length){
+    }
+    else {
+      let a = this.todos.splice(index, 1);
+    }
+  }
+  update(index, updatedTodo){
+    if(index >= this.todos.length){
+    }
+    else {
+      if(updatedTodo == "Invalid Task"){
+      }
+      else{
+        this.todos[index] = updatedTodo;
+      }
+    }
+  }
+  getAll(){
+    return this.todos;
+  }
+  get(index){
+    if(index >= this.todos.length){
+      return null;
+    }
+    else {
+      let a = this.todos[index];
+      return a;
+    }
+  }
+  clear(){
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
