a6f15ba2cabf808e7fa0e6f300c0aa202602873e
Mayank-8127 <145133998+Mayank-8127@users.noreply.github.com>
2024-03-29 17:52:52 +0530

    Updated assignments/week-2/01-async-js/hard(promises)/3-promise-all.js

--- assignments/week-2/01-async-js/hard (promises)/3-promise-all.js
+++ assignments/week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -4,35 +4,48 @@
  * Return a promise.all which return the time in milliseconds it takes to complete the entire operation.
  */
 
-function wait1(t) {
+function wait1(t1) {
     return new Promise(function(resolve){
         setTimeout(function(){
             resolve()
-        }, t*1000)
+        }, t1*1000)
     })
 
 }
 
-function wait2(t) {
+function wait2(t2) {
     return new Promise(function(resolve){
         setTimeout(function(){
             resolve()
-        }, t*1000)
+        }, t2*1000)
     })
 
 }
 
-function wait3(t) {
+function wait3(t3) {
     return new Promise(function(resolve){
         setTimeout(function(){
             resolve()
-        }, t*1000)
+        }, t3*1000)
     })
 
+}
+
+function promiseAll(t1, t2, t3){
+    return Promise.all([wait1(t1),wait2(t2),wait3(t3)])
+}
+
+let c = new Date();
+
+function onDone(){
+    let b = new Date();
+    return (b.getTime() - c.getTime())
 }
 
 function calculateTime(t1, t2, t3) {
-
+    promiseAll(t1, t2, t3).then(onDone)
 }
 
 module.exports = calculateTime;
+
+console.log(calculateTime(1,3,5))
