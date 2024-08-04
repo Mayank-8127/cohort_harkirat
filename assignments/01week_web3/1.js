f4a7c42d65221c92f5b1b0771fd553aecf81bef9
Mayank-8127 <145133998+Mayank-8127@users.noreply.github.com>
2024-08-05 04:10:12 +0530

    01week_web3 assignment block in a blockchain

---
+++ assignments/01week_web3/1.js
@@ -0,0 +1,14 @@
+const crypto = require('crypto');
+let hash = '1';
+let input = 0;
+const prefix = 'Mayank'
+while(true){
+    input++;
+    let input_s = prefix + input.toString();
+    hash = crypto.createHash('sha256').update(input_s).digest('hex');
+    if(hash.startsWith('00000')){
+        break;
+    }
+}
+console.log(hash)
+console.log(input)
